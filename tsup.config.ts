543938a2d06ffd5420d07cfb5228254a8d042936
Juniors Chagas <jjuniorchagas15@gmail.com>
2026-04-10 09:59:07 -0300

    fix: esm modules runtime error on pnpm and npm

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm'],
+  format: ['cjs'],
   target: 'node18',
   bundle: true,
   outDir: 'dist',
